72e70b2afeb0759ce7e0e46484b5e5fae88019ce
denerval1111 <denerval1111@gmail.com>
2025-05-08 10:07:45 -0300

    Update layout.tsx

--- desafio-vitalidade/app/layout.tsx
+++ desafio-vitalidade/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { defaultMetadata, generateMetadata } from '@/lib/metadata';
+import metadata from './lib/metadata';
 import './globals.css';
 import './responsive.css';
 import { Inter } from 'next/font/google';
